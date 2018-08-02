df01d9de4416eb0e39beff032d20133ebd6f7f4a
Unknown <gabebarber94@gmail.com>
2018-08-02 16:11:21 -0600

    Added a lot of functionality

---
+++ project/Chess/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Chess
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+           DialogResult msgBox = MessageBox.Show("Chess960 Options", "Play with chess960 rules?", MessageBoxButtons.YesNo);
+            if(msgBox == DialogResult.Yes)
+            {
+
+            }
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
+    }
+}
